2b6accbebd3bf9021d3112a3229c27239efdbc43
Alejo1707 <pianistaalex1990@hotmail.es>
2025-07-08 11:07:45 -0500

    Renamde alice66.h to board.h, changed headers from board.h and mcuconf.h

--- keyboards/mlice66/mcuconf.h
+++ keyboards/mlice66/mcuconf.h
@@ -1,21 +1,17 @@
-#ifndef MCUCONF_H
-    #define MCUCONF_H
+#pragma once
+#include_next <mcuconf.h>
 
-    #define STM32F4xx_MCUCONF
-    #define STM32F401_MCUCONF
+#define STM32_HSE_ENABLED TRUE
+#define STM32_PLLSRC STM32_PLLSRC_HSE
 
-    #define STM32_HSE_ENABLED TRUE
-    #define STM32_PLLSRC STM32_PLLSRC_HSE
+#undef STM32_PLLM_VALUE
+#define STM32_PLLM_VALUE 14
 
-    #undef STM32_PLLM_VALUE
-    #define STM32_PLLM_VALUE 14
+#undef STM32_PLLN_VALUE
+#define STM32_PLLN_VALUE 336
 
-    #undef STM32_PLLN_VALUE
-    #define STM32_PLLN_VALUE 336
+#undef STM32_PLLP_VALUE
+#define STM32_PLLP_VALUE 4
 
-    #undef STM32_PLLP_VALUE
-    #define STM32_PLLP_VALUE 4
-
-    #undef STM32_PLLQ_VALUE
-    #define STM32_PLLQ_VALUE 8
-#endif
+#undef STM32_PLLQ_VALUE
+#define STM32_PLLQ_VALUE 8
--- keyboards/mlice66/mlice66.h
+++
@@ -1,7 +0,0 @@
-#ifndef MLICE66_H
-    #define MLICE66_H
-    #ifdef STM32_HSECLK
-        #undef STM32_HSECLK
-    #endif
-    #define STM32_HSECLK 16000000U
-#endif
